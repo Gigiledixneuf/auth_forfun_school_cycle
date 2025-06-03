38e0851751ce54d7f612ad1fbe7b80548fa103be
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-06-03 02:49:12 +0100

    feat : creation du composant single annonce et implementation de la logique

---
+++ src/app/pages/announcement/announcement-single/announcement-single.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { AnnouncementService } from '../../../core/services/announcement/announcement.service';
+import { Announcement } from '../../../core/models/announcement/announcement';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-announcement-single',
+  imports: [NgFor, NgIf, RouterLink, CommonModule],
+  templateUrl: './announcement-single.component.html',
+  styleUrl: './announcement-single.component.css',
+})
+export class AnnouncementSingleComponent {
+  constructor(
+    private annoncementService: AnnouncementService,
+    private route: ActivatedRoute
+  ) {}
+  storageUrl = environment.storageUrl;
+  currentImage: string = '';
+  announcement!: Announcement;
+  similarAnnouncements: Announcement[] = [];
+  articleId: number = -1;
+
+  ngOnInit() {
+    //recharger la page en dunction du nouvel id
+    this.route.params.subscribe(params => {
+      this.articleId = +params['id'];
+      this.getSingleAnnouncement();
+      this.getSimilarAnnouncement();
+    });
+  }
+
+  
+  //function pour recuperr l'annonce en detail
+  getSingleAnnouncement(){
+    this.articleId = Number(this.route.snapshot.paramMap.get('id'));
+
+    console.log("Id de l'annonce : ", this.articleId);
+
+    this.annoncementService.getAnnoucement(this.articleId).subscribe({
+      next: (res) => {
+        this.announcement = res.data;
+        console.log('Annonce récupérée :', this.announcement);
+        // Vérifie que des photos existent, et affecte currentImage
+        if (this.announcement.photos && this.announcement.photos.length > 0) {
+          this.currentImage = this.announcement.photos[0].url;
+        }
+      },
+      error: (err) => {
+        console.error("Erreur lors du chargement de l'annonce :", err);
+      },
+    });
+  }
+  
+
+  //function pour recuperr les articles similaires 
+  getSimilarAnnouncement() {
+    this.annoncementService.getSimilarAnnouncements(this.articleId).subscribe(
+      (res) => {
+        this.similarAnnouncements = res.data;
+        console.log('annonces similaires', this.similarAnnouncements);
+      },
+      (error) => {
+        console.error('Erreur de récupération des annonces similaires', error);
+      }
+    );
+  }
+  
+  //function pour recuperr changer la photo principale de l'annonce
+  changeMainImage(url: string) {
+    this.currentImage = url;
+  }
+}
